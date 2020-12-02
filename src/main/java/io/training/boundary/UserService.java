7e5a8ec1a7f6185bab2b770709cac137f0881bcf
symoh <smnkimathi@gmail.com>
2020-12-02 15:21:43 +0300

    added find user by mail and username methods

--- src/main/java/io/training/boundary/UserService.java
+++ src/main/java/io/training/boundary/UserService.java
@@ -2,6 +2,10 @@ package io.training.boundary;
 
 import io.training.entity.User;
 
+import java.util.Optional;
+
 public interface UserService extends AbstractBean<User,Long>{
 
+    Optional<User> getUserByEmail(String email);
+    Optional<User> getUserByUsername(String username);
 }
--- src/main/java/io/training/boundary/impl/UserServiceImpl.java
+++ src/main/java/io/training/boundary/impl/UserServiceImpl.java
@@ -3,9 +3,15 @@ package io.training.boundary.impl;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import io.training.boundary.UserService;
 import io.training.entity.User;
 import io.training.util.Constants;
+
+import java.util.List;
+import java.util.Optional;
+
 @Stateless
 public class UserServiceImpl extends AbstractBeanImpl<User, Long> implements UserService {
   @PersistenceContext(name = Constants.ENTITY_MANAGER_NAME)
@@ -17,5 +23,29 @@ public class UserServiceImpl extends AbstractBeanImpl<User, Long> implements Use
   @Override
   protected EntityManager getEntityManager() {
     return entityManager;
+  }
+
+  @Override
+  public Optional<User> getUserByEmail(String email) {
+    TypedQuery<User> userTypedQuery =getEntityManager()
+            .createQuery("select u from User where email= : email",User.class)
+            .setParameter("email",email);
+    List<User> userList=userTypedQuery.getResultList();
+    if(userList.size()>0){
+      return Optional.of(userList.get(0));
+    }
+      return Optional.empty();
+  }
+
+  @Override
+  public Optional<User> getUserByUsername(String username) {
+    TypedQuery<User> userTypedQuery =getEntityManager()
+            .createQuery("select u from User where username= : username",User.class)
+            .setParameter("email",username);
+    List<User> userList=userTypedQuery.getResultList();
+    if(userList.size()>0){
+      return Optional.of(userList.get(0));
+    }
+    return Optional.empty();
   }
 }
