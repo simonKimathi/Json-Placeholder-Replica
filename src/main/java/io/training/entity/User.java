82e03d95fa78b948c22bf2ec3b30a650c3a2ea7e
symoh <smnkimathi@gmail.com>
2020-12-10 10:04:42 +0300

    updated user entity

--- src/main/java/io/training/entity/User.java
+++ src/main/java/io/training/entity/User.java
@@ -1,8 +1,12 @@
 package io.training.entity;
 
 import java.util.Objects;
+import java.util.Set;
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,30 +19,55 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@JsonIgnoreProperties({"posts"})
 public class User {
 
-  @Column(name="USER_ID")
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
+
   @Column
+  @NotEmpty
+  @NotNull
   private String name;
+
   @Column(unique = true)
+  @NotEmpty
+  @NotNull
   private String username;
+
   @Column(unique = true)
+  @NotEmpty
+  @NotNull
   private String email;
+
   @Embedded
   Address address;
+
   @Column
+  @NotEmpty
+  @NotNull
   private String phone;
+
   @Column
   private String website;
+
   @Embedded
   private Company company;
 
   @Column
   @Enumerated(EnumType.STRING)
   private DeleteStatus deleteStatus;
+
+  @OneToMany(mappedBy ="user", fetch = FetchType.LAZY,
+          cascade = CascadeType.ALL,orphanRemoval = true)
+  private Set<Post> posts;
+
+  public static User fromId(long userId){
+    User user=new User();
+    user.id=userId;
+    return user;
+  }
 
 
 
