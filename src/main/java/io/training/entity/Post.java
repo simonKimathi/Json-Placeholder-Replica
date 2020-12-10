7a0fe8ccaf5833a614c3e994f1704bf948907074
symoh <smnkimathi@gmail.com>
2020-12-10 10:35:09 +0300

    updated post entity

--- src/main/java/io/training/entity/Post.java
+++ src/main/java/io/training/entity/Post.java
@@ -1,33 +1,65 @@
 package io.training.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.*;
+import lombok.*;
+
+import java.security.PublicKey;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 @Setter
 @Getter
-@ToString
+@ToString(exclude = {"user"})
 @EqualsAndHashCode
+@JsonIgnoreProperties({"comments"})
 public class Post {
   @Column(name="POST_ID")
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private int id;
-  @ManyToOne(fetch = FetchType.EAGER)
-  User user;
+  private long id;
+
   @Column
+  @NotEmpty
+  @NotNull
   private String title;
-  @Column
+
+  @Lob
+  @NotEmpty
+  @NotNull
   private String body;
 
-  @Column(length = 255, columnDefinition = "varchar(255) default 'AVAILABLE'")
+  @Column
   @Enumerated(EnumType.STRING)
   private DeleteStatus deleteStatus;
+
+
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "user_id" , nullable = false)
+  @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+  @JsonProperty("userId")
+  @Setter(AccessLevel.NONE)
+  private User user;
+
+  @OneToMany(mappedBy = "post" ,fetch = FetchType.LAZY,
+             cascade = CascadeType.ALL,orphanRemoval = true)
+  private Set<Comment> comments;
+
+  @JsonProperty("userId")
+  public void setUserById(Long userId){
+    user=User.fromId(userId);
+  }
+
+  public static Post fromId(Long postId){
+    Post post=new Post();
+    post.setId(postId);
+    //post.id=postId;
+    return post;
+  }
 
 }
