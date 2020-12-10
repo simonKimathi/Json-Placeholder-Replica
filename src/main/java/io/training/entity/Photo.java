5bafe528f595d57d4618bb35f911fd2196fe66f2
symoh <smnkimathi@gmail.com>
2020-12-10 10:47:46 +0300

    added photos entity

---
+++ src/main/java/io/training/entity/Photo.java
@@ -0,0 +1,54 @@
+package io.training.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table
+@ToString(exclude = {"album"})
+@Getter
+@Setter
+@NoArgsConstructor
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    @NotEmpty
+    @NotNull
+    private String title;
+
+    @Column
+    @NotEmpty
+    @NotNull
+    private String url;
+
+    @Column
+    @NotEmpty
+    @NotNull
+    private String thumbnailUrl;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "album_id", nullable = false)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("albumId")
+    @Setter(AccessLevel.NONE)
+    private Album album;
+
+    @JsonProperty("albumId")
+    public void setAlbumById(long albumId) {
+        album = Album.fromId(albumId);
+    }
+
+}
